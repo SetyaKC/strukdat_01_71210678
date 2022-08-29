a613391ad329dd801cac96d6ab2f1dcace9b9f02
SetyaKC <93339520+SetyaKC@users.noreply.github.com>
2022-08-29 18:44:09 +0700

    Add files via upload

---
+++ praktikum minggu 1.py
@@ -0,0 +1,52 @@
+# print("hello Word!")
+
+#python interpreter
+# a=10
+# b=20
+# c=30
+# print(a)
+# print(b)
+# print(c)
+
+# a = 10
+# b = "Selamat Pagi"
+# c = 30
+# print(a)
+# print(b)
+# print(c)
+
+# a = 10
+# b = 20
+# c = 30
+# print(a%b)
+# print(b-c)
+# print(c+a+b)
+
+# a = int(input("Masukkan angka: "))
+# b = int(input("Masukkan angka: "))
+# if a>b:
+#     print(a)
+# elif b>a:
+#     print(b)
+# else:
+#     print("sama")
+
+# a = int(input("Masukkan angka: "))
+# b = int(input("Masukkan angka: "))
+# print(a+b)
+
+# a = int(input("Masukkan angka: "))
+# b = int(input("Masukkan angka: "))
+# if a>b and a>0:
+#     print(a)
+# elif b>a:
+#     print(b)
+# else:
+#     print("sama")
+
+# def input_nama():
+#     nama = input("Masukkan nama: ")
+#     print ("hai",nama)
+# for i in range(1,10):
+
+a = int(input("angka: "))
